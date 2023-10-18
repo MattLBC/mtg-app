84d17975a65165998d614879dc60fdb9ef7ca9e8
Matthew Conolly <matthew@conolly.uk>
2023-10-18 23:54:53 +0100

    Combines both L+R click to be managed by the same function

--- app/commanderDamage.tsx
+++ app/commanderDamage.tsx
@@ -4,13 +4,22 @@ import React, { useState } from "react";
 export default function CommanderDamage() {
   const [commanderDamage, setCommanderDamage] = useState<number>(0);
 
+  const commanderDamageClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (event.button === 0) {
+      setCommanderDamage(commanderDamage + 1);
+    } else {
+      setCommanderDamage(commanderDamage - 1);
+    }
+  };
+
   return (
     <div>
       <h1>Commander Damage</h1>
       <button
         className="btn-blue-pill"
-        onClick={() => setCommanderDamage(commanderDamage + 1)}
-        onAuxClick={() => setCommanderDamage(commanderDamage - 1)}
+        onClick={commanderDamageClick}
+        onContextMenu={commanderDamageClick}
       >
         {commanderDamage}
       </button>
