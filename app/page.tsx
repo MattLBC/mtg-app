eaa11555faac96040f7fbb61116c539f70055fe6
Matthew Conolly <matthew@conolly.uk>
2023-10-30 19:27:09 +0000

    Adds beginings of socket inferstructure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import { socket } from '../socket';
+import { Events } from "./Events";
+import { ConnectionState } from './ConnectionState';
+import { ConnectionManager } from './ConnectionManager';
 import PlayerCard from "./playerCard";
 
+
 export default function Home() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents([...fooEvents, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
   return (
     <main>
       <h1>MAIN</h1>
+      <ConnectionState isConnected={ isConnected } />
+      <Events events={ fooEvents } />
+      <ConnectionManager />
       <PlayerCard />
     </main>
   );
---
+++ server.js
@@ -0,0 +1,25 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  });
+  
+  io.on('connection', socket => {
+    console.log(`connect: ${socket.id}`);
+  
+    socket.on('hello!', () => {
+      console.log(`hello from ${socket.id}`);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log(`disconnect: ${socket.id}`);
+    });
+  });
+  
+  io.listen(3001);
+  
+  setInterval(() => {
+    io.emit('message', new Date().toISOString());
+  }, 1000);
