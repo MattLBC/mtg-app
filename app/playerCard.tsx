f053308af17117ff514f2c16beda7afb710fe96c
Matthew Conolly <matthew@conolly.uk>
2023-11-15 13:47:55 +0000

    Adds basic player name support

--- app/playerCard.tsx
+++ app/playerCard.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import LifeCounter from "./lifeCounter";
 import CommanderDamage from "./commanderDamage";
 
 export default function PlayerCard() {
+  const [playerName, setPlayerName] = useState<string>("Player Name");
+  const [newPlayerName, setNewPlayerName] = useState<string>("");
+
+  const handlePlayerName = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setPlayerName(newPlayerName);
+    setNewPlayerName("");
+  };
+
   return (
     <div>
-      <h1>PLAYER CARD</h1>
+      <h1>{playerName}</h1>
+      <form onSubmit={handlePlayerName}>
+        <input
+          type="text"
+          value={newPlayerName}
+          placeholder="Change Name"
+          onChange={(event) => setNewPlayerName(event.target.value)}
+        ></input>
+        <button className="btn-blue-pill" type="submit">
+          Edit
+        </button>
+      </form>
       <LifeCounter />
       <CommanderDamage />
     </div>
--- app/setPlayers.tsx
+++ app/setPlayers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 
 interface SetPlayersProps {
   setPlayerNumber: React.Dispatch<React.SetStateAction<number>>;
