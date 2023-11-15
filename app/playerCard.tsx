4cb7acf31dcf5172aed6922a5565bb371f6f7820
Matthew Conolly <matthew@conolly.uk>
2023-11-15 14:21:21 +0000

    Adds a toggle for name form

--- app/playerCard.tsx
+++ app/playerCard.tsx
@@ -5,27 +5,42 @@ import CommanderDamage from "./commanderDamage";
 export default function PlayerCard() {
   const [playerName, setPlayerName] = useState<string>("Player Name");
   const [newPlayerName, setNewPlayerName] = useState<string>("");
+  const [showNameForm, setShowNameForm] = useState<boolean>(false);
 
   const handlePlayerName = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (newPlayerName === "") {
+      setShowNameForm(false);
+      return;
+    }
     setPlayerName(newPlayerName);
     setNewPlayerName("");
+    setShowNameForm(false);
   };
+
+  function togglePlayerNameForm() {
+    setShowNameForm(!showNameForm);
+  }
 
   return (
     <div>
       <h1>{playerName}</h1>
-      <form onSubmit={handlePlayerName}>
-        <input
-          type="text"
-          value={newPlayerName}
-          placeholder="Change Name"
-          onChange={(event) => setNewPlayerName(event.target.value)}
-        ></input>
-        <button className="btn-blue-pill" type="submit">
-          Edit
-        </button>
-      </form>
+
+      {showNameForm ? (
+        <form onSubmit={handlePlayerName} id="playerNameForm">
+          <input
+            type="text"
+            value={newPlayerName}
+            placeholder="Change Name"
+            onChange={(event) => setNewPlayerName(event.target.value)}
+          ></input>
+          <button className="btn-blue-pill" type="submit">
+            Edit
+          </button>
+        </form>
+      ) : (
+        <button onClick={() => togglePlayerNameForm()}>Change name</button>
+      )}
       <LifeCounter />
       <CommanderDamage />
     </div>
