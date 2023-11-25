93d525245e1e3db7f6f11907078fe6f55aea86e7
Matthew Conolly <matthew@conolly.uk>
2023-11-25 20:49:11 +0000

    Fixes issue with name boxes

--- app/playerCard.tsx
+++ app/playerCard.tsx
@@ -35,11 +35,44 @@ export default function PlayerCard({
     setShowNameForm(!showNameForm);
   }
 
+  function showPlayerNameInput() {
+    if (document.getElementById(`playerNameForm${individualPlayerNumber}`).style.display === "block") {
+      document.getElementById(`playerNameForm${individualPlayerNumber}`).style.display = "none";
+      return;
+    }
+    document.getElementById(`playerNameForm${individualPlayerNumber}`).style.display = "block";
+  }
+
   return (
     <div>
       <div className="PlayerNameBox">
         <h1 className="playerName">{playerName}</h1>
-        {showNameForm ? (
+        <button
+          onClick={() => showPlayerNameInput()}
+          className={`playerButton${individualPlayerNumber} playerNameChange dropdown`}
+        >
+          <FontAwesomeIcon icon={faPencil} className="faButtonIcon" />
+        </button>
+        <form
+          onSubmit={handlePlayerName}
+          id={`playerNameForm${individualPlayerNumber}`}
+          className="dropdown-content"
+        >
+          <input
+            type="text"
+            value={newPlayerName}
+            placeholder="Change Name"
+            onChange={(event) => setNewPlayerName(event.target.value)}
+          ></input>
+          <button
+            className={`playerButton${individualPlayerNumber}`}
+            type="submit"
+          >
+            <FontAwesomeIcon icon={faPencil} className="faButtonIcon" />
+          </button>
+        </form>
+
+        {/* {showNameForm ? (
           <form onSubmit={handlePlayerName}>
             <input
               type="text"
@@ -57,11 +90,11 @@ export default function PlayerCard({
         ) : (
           <button
             onClick={() => togglePlayerNameForm()}
-            className={`playerButton${individualPlayerNumber} playerNameChange`}
+            className={`playerButton${individualPlayerNumber} playerNameChange dropdown`}
           >
             <FontAwesomeIcon icon={faPencil} className="faButtonIcon" />
           </button>
-        )}
+        )} */}
       </div>
       <div className="PlayerArea">
         <CommanderSummon index={null} />
